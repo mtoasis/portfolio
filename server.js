d8a71753a16106401cd4161f384367bed6ba3e53
mtoasis <mtoasis22@gmail.com>
2018-04-12 23:13:39 -0700

    bug fixed
    bug fixed

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
+const path = require('path');
 
 
 const app = express();
@@ -9,11 +10,11 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static("public"));
-
-// require("./routes/apiRoutes.js")(app);
-
-// app.listen
+// app.use(express.static("build"));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 app.listen(PORT, function() {
